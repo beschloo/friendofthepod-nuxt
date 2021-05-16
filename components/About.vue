8f03ee2873bae35ce96c91bfe6326bdadb557fdb
Brennan Schloo <brennan.schloo@gmail.com>
2021-05-16 10:38:33 -0700

    about content pulling from forestry

--- components/About.vue
+++ components/About.vue
@@ -1,52 +1,31 @@
 <template>
-        <section class="info_blurb">
-            <div v-html="data.description"></div>
-            <div v-html="data.cta"></div>
-            <ul>
-                <li>
-                    <p>
-                        <a :href="`mailto:${data.contact.email}`">Email: {{ data.contact.email }}</a>
-                    </p>
-                </li>
-                <li>
-                    <p>
-                        <a :href="`https://twitter.com/${data.contact.twitter_handle}`">Twitter: @{{ data.contact.twitter_handle }}</a>
-                    </p>
-                </li>
-                <li>
-                    <p>
-                        <a :href="`https://github.com/${data.contact.github_handle}`">Github: {{ data.contact.github_handle }}</a>
-                    </p>
-                </li>
-            </ul>
-        </section>
+  <section class="info_blurb">
+    <div v-if="data" v-html="data.about"></div>
+  </section>
 </template>
 
 <script>
-    export default {
-        layout: 'layout',
-        computed: {
-            isInfoPage() {
-                return this.$nuxt._route.name === "info" && true
-            }
-        },
-        async asyncData() {
-            try {
-                const data = await import(`~/content/data/about.json`);
-                return {
-                data
-                }
-            } catch(err) {
-                return false
-            }
-        },
-        head() {
-            return {
-                bodyAttrs: {
-                    style: this.isInfoPage && `background-color: ${this.data.background_color}; color: ${this.data.text_color}`
-                }
-            }
-        }
+export default {
+  layout: 'layout',
+  data() {
+    return {
+      data: {
+        about: '',
+      },
     }
+  },
+  async fetch() {
+    try {
+      const data = await import(`~/content/data/about.json`)
+      this.data = data
+    } catch (err) {
+      return false
+    }
+  },
+  computed: {
+    isInfoPage() {
+      return this.$nuxt._route.name === 'info' && true
+    },
+  },
+}
 </script>
-
