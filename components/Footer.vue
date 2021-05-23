f856dbdb73b0592e834fe638f78861dc6b41d709
angelaarnett <52254111+angelaarnett@users.noreply.github.com>
2021-05-22 21:16:59 -0700

    first pass at footer layout

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,79 @@
 <template>
-  <section id="contact-start"></section>
+  <section id="contact-start">
+    <div class="contact-contents">
+      <div class="contact-section_left">
+        <div class="fotp-logo">
+          <img
+            src="~assets/images/logo.svg"
+            class="fotp-logo_footer"
+            alt="logo"
+          />
+        </div>
+        <div class="contact-details">
+          <div class="contact-title">CONTACT</div>
+          <div class="phone">
+            <div class="phone-logo"></div>
+            <div class="phone-number">504-621-7648</div>
+          </div>
+          <div class="email">
+            <div class="email-logo"></div>
+            <div class="email-address">jonathan@friendofthepod.org</div>
+          </div>
+        </div>
+      </div>
+      <div class="contact-section_right">
+        <div class="ig"></div>
+        <div class="fb"></div>
+      </div>
+    </div>
+    <div class="copyright">
+      <div class="copyright-copy">Copyright 2021, Friend of the Pod</div>
+    </div>
+  </section>
 </template>
 
-<style lang="scss"></style>
+<style scoped>
+#contact-start {
+  display: flex;
+  flex-direction: column;
+  background-color: #c1918b;
+  height: 372px;
+  font-family: rock;
+}
+.contact-contents {
+  flex-direction: row;
+}
+.contact-section_left {
+}
+.fotp-logo {
+}
+.fotp-logo img {
+  width: 168px;
+}
+.contact-details {
+}
+.contact-title {
+}
+.phone {
+}
+.phone-logo {
+}
+.phone-number {
+}
+.email {
+}
+.email-logo {
+}
+.email-address {
+}
+.contact-section_right {
+}
+.ig {
+}
+.fb {
+}
+.copyright {
+}
+.copyright-copy {
+}
+</style>
--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo">
-    <img src="~assets/images/logo.png" class="fotp-logo" alt="logo" />
+    <img src="~assets/images/logo.svg" class="fotp-logo" alt="logo" />
   </div>
 </template>
 
