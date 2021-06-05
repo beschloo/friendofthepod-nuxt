e02f7e6b31ee8f00bec5cec92489f438ba6a098f
angelaarnett <52254111+angelaarnett@users.noreply.github.com>
2021-06-04 20:57:10 -0700

    title change final

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'friend of the pod',
+    title: 'Friend of the Pod',
     htmlAttrs: {
       lang: 'en',
     },
